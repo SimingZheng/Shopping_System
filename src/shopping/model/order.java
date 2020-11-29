ab1bd1c15d72263d9c081cb3a4e940c1bfa7420b
whoiskoala <35552935+whoiskoala@users.noreply.github.com>
2020-11-29 11:13:57 +0800

    update order

--- src/shopping/model/order.java
+++ src/shopping/model/order.java
@@ -1,4 +1,33 @@
 package shopping.model;
 
+import java.util.ArrayList;
+
+
 public class order {
+    ArrayList<product> list=new ArrayList<product>();
+
+    double total;
+    public order(ArrayList<product> orderlist, double total) {
+        this.list=orderlist;
+        this.total=total;
+    }
+
+    public ArrayList<product> getList() {
+        return list;
+    }
+
+
+
+    public void setList(ArrayList<product> list) {
+        this.list = list;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
 }
+
